3c90252fc1f968917a7fbf5540fbaab0a0158ed6
Pro7Prabhakar <prabhashankarmishra202@gmail.com>
2023-12-27 13:32:02 +0530

    few modifications

--- pokemon.js
+++ pokemon.js
@@ -5,7 +5,8 @@ const url = "https://pokeapi.co/api/v2/pokemon/"
 
 button.addEventListener('click',(() =>
 {
-    const search = inputText.value
+    const text = inputText.value
+    const search = text.toLowerCase();
     fetchData(url,search)
 }
 ))
@@ -26,7 +27,10 @@ async function fetchData(url,search)
     }
     catch(err)
     {
+        pokelist.innerHTML = ''
+        document.body.style.backgroundImage = "url('./media/giphy.gif')";
         console.error(err)
+        pokelist.innerHTML = `<strong>No Pokemon Found with that Name. Search Again</strong>`
     }
 }
 function displayData(data,response)
@@ -36,14 +40,14 @@ function displayData(data,response)
 
     p.push(data)
     const pokeValue = document.createElement('div')
-    // <img src=${p[0].sprites.front_default} alt=${p[0].name} />
+    pokeValue.classList.add('pokeValue')
     pokeValue.innerHTML = `
     <img src=${p[0].sprites.front_default} alt=${p[0].name} />
     <h2>Name: ${p[0].name}</h2>
     <p>Pokemon Type: ${p[0].types[0].type.name}
+    <p>Stat: ${p[0].stats[0].base_stat}
     <p>Height: ${p[0].height}</p>
     <p>Weight: ${p[0].weight}</p>
-    <p>Stat: ${p[0].stats[0].base_stat}
     `
     backgroundImg(p[0].types[0].type.name)
     console.log(pokeValue,response)
@@ -51,54 +55,51 @@ function displayData(data,response)
 }
 function backgroundImg(type){
     if (type == 'electric'){
-        document.body.style.backgroundImage = "url('./electric.gif')";
+        document.body.style.backgroundImage = "url('./media/electric.gif')";
     }
     else if (type == 'fire'){
-        document.body.style.backgroundImage = "url('./fire.gif')";
+        document.body.style.backgroundImage = "url('./media/fire.gif')";
     }
     else if (type == 'grass'){
-        document.body.style.backgroundImage = "url('./grass.gif')";
+        document.body.style.backgroundImage = "url('./media/grass.gif')";
     }
     else if (type == 'rock'){
-        document.body.style.backgroundImage = "url('./rock.gif')";
+        document.body.style.backgroundImage = "url('./media/rock.gif')";
     }
     else if (type == 'dragon'){
-        document.body.style.backgroundImage = "url('./dragon.gif')";
+        document.body.style.backgroundImage = "url('./media/dragon.gif')";
     }
     else if (type == 'water'){
-        document.body.style.backgroundImage = "url('./water.gif')";
+        document.body.style.backgroundImage = "url('./media/water.gif')";
     }
     else if (type == 'ice'){
-        document.body.style.backgroundImage = "url('./ice.gif')";
+        document.body.style.backgroundImage = "url('./media/ice.gif')";
     }
     else if (type == 'fighting'){
-        document.body.style.backgroundImage = "url('./fighting.gif')";
+        document.body.style.backgroundImage = "url('./media/fighting.gif')";
     }
     else if (type == 'normal'){
-        document.body.style.backgroundImage = "url('./normal.gif')";
+        document.body.style.backgroundImage = "url('./media/normal.gif')";
     }
     else if (type == 'psychic'){
-        document.body.style.backgroundImage = "url('./psychic.gif')";
+        document.body.style.backgroundImage = "url('./media/psychic.gif')";
     }
     else if (type == 'ghost'){
-        document.body.style.backgroundImage = "url('./ghost.gif')";
+        document.body.style.backgroundImage = "url('./media/ghost.gif')";
     }
     else if (type == 'dark'){
-        document.body.style.backgroundImage = "url('./dark.gif')";
+        document.body.style.backgroundImage = "url('./media/dark.gif')";
     }
     else if (type == 'poison'){
-        document.body.style.backgroundImage = "url('./poison.gif')";
+        document.body.style.backgroundImage = "url('./media/poison.gif')";
     }
     else if (type == 'bug'){
-        document.body.style.backgroundImage = "url('./bug.gif')";
+        document.body.style.backgroundImage = "url('./media/bug.gif')";
     }
     else if (type == 'fairy'){
-        document.body.style.backgroundImage = "url('./fairy.gif')";
-    }
-    else if (response.ok === false){
-        document.body.style.backgroundImage = "url('./giphy.gif')";
+        document.body.style.backgroundImage = "url('./media/fairy.gif')";
     }
     else{
-        document.body.style.backgroundImage = "url('./giphy.gif')";
+        document.body.style.backgroundImage = "url('./media/giphy.gif')";
     }
 }
